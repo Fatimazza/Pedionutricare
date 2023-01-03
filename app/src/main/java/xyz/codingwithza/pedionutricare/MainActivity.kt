e70a54ecc7502771c22a75d8cf5acf350f993177
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-03 16:39:31 +0700

    Home - Remove Box Layout

--- app/src/main/java/xyz/codingwithza/pedionutricare/MainActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/MainActivity.kt
@@ -3,13 +3,10 @@ package xyz.codingwithza.pedionutricare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
@@ -36,23 +33,14 @@ fun PedionutricareApp() {
             )
         }
     ) {
-        MainContent(innerPadding = it)
+        MainContent()
     }
 }
 
 @Composable
 fun MainContent(
-    innerPadding: PaddingValues
 ){
-    Box(
-        modifier = Modifier
-            .padding(innerPadding)
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    )
-    {
-        Text("Hello World")
-    }
+
 }
 
 @Preview(showBackground = true)
