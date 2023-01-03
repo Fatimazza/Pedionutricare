75d07491f5061830bb543686a59f92e7d7c1df1d
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-03 14:25:28 +0700

    Home - Add Top App Bar containing Application title

--- app/src/main/java/xyz/codingwithza/pedionutricare/MainActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/MainActivity.kt
@@ -3,15 +3,14 @@ package xyz.codingwithza.pedionutricare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
 
@@ -29,7 +28,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun PedionutricareApp() {
     Scaffold(
-
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(stringResource(R.string.app_name))
+                }
+            )
+        }
     ) {
         MainContent(innerPadding = it)
     }
