64a2af1a2dfa6f3e87ad636287d0659896bb6413
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-03 16:43:23 +0700

    Home - Use Lazy Column Layout

--- app/src/main/java/xyz/codingwithza/pedionutricare/MainActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/MainActivity.kt
@@ -3,12 +3,19 @@ package xyz.codingwithza.pedionutricare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,8 +46,17 @@ fun PedionutricareApp() {
 
 @Composable
 fun MainContent(
-){
-
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier
+            .padding(8.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+    }
 }
 
 @Preview(showBackground = true)
