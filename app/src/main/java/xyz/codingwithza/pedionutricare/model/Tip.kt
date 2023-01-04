184563048d271ccc3984c8671de2ca17ab8c4513
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-04 14:10:14 +0700

    Model - Add data class and data source for Tips

---
+++ app/src/main/java/xyz/codingwithza/pedionutricare/model/Tip.kt
@@ -0,0 +1,8 @@
+package xyz.codingwithza.pedionutricare.model
+
+data class Tip(
+    val id: Int,
+    val title: String,
+    val desc: String,
+    val point: String
+)
---
+++ app/src/main/java/xyz/codingwithza/pedionutricare/model/TipDataSource.kt
@@ -0,0 +1,114 @@
+package xyz.codingwithza.pedionutricare.model
+
+object TipDataSource {
+    val tipItems = listOf(
+        Tip(
+            1,
+            "Penurunan Nafsu Makan",
+            "",
+            "1) Mencoba mengubah waktu pemberian makan, tempat dan suasana makan \n" +
+                    "2) Biarkan anak membantu belanja dan mempersiapkan makanan \n" +
+                    "3) Berikan makanan yang padat gizi (tinggi energi dan protein) dan snack \n" +
+                    "4) Jangan memaksa anak untuk makan, " +
+                    "ini mungkin dapat menyebabkan nafsu makannya menurun \n" +
+                    "5) Menjadikan waktu makan menyenangkan \n"
+        ),
+        Tip(
+            2,
+            "Mual/Muntah",
+            "",
+            "1) Memberikan makanan tinggi karbohidrat (crackers, roti panggang, biskuit) " +
+                    "atau jus buah yang tidak asam \n" +
+                    "2) Berikan makanan dalam porsi kecil dan sering\n \n" +
+                    "3) Hidari makanan yang terlalu manis atau makanan yang tinggi lemak\n \n" +
+                    "4) Hidari ruangan yang terlalu panas atau ruangan yang dipenuhi bau makanan " +
+                    "yang mungkin merangsang mual\n \n" +
+                    "5) Ingatkan anak untuk sering minum cairan secara perlahan, " +
+                    "penggunaan sedotan mungkin membantu \n" +
+                    "6) Berikan waktu istirahat setelah makan \n" +
+                    "7) Hindari memberikan makanan pada saat mual karena dapat menyebabkan " +
+                    "anak tidak suka makanan tersebut secara permanen (trauma). \n"
+        ),
+        Tip(
+            3,
+            "Diare",
+            "",
+            "1) Memberikan diet rendah lemak rendah serat " +
+                    "dan rendah laktosa (batasi susu maksimal 2 gelas per hari) \n" +
+                    "2) Menyajikan makanan dalam kondisi suhu ruang karena makanan " +
+                    "yang panas dapat mempercepat pengosongan lambung \n" +
+                    "3) Berikan cairan yang cukup untuk mencegah dehidrasi \n"
+        ),
+        Tip(
+            4,
+            "Sembelit/Susah BAB",
+            "",
+            "1) Berikan cairan yang cukup \n" +
+                    "2) Berikan minuman yang hangat untuk meningkatkan aktivitas lambung \n" +
+                    "3) Berikan makanan tinggi serat (sayur, buah dan kacang-kacangan) \n"
+        ),
+        Tip(
+            5,
+            "Kesulitan Mengunyah/Menelan",
+            "Kesulitan mengunyah dan menelan mungkin terjadi karena efek samping terapi " +
+                    "yang menyebabkan sariawan di mulut dan tenggorokan, karena itu dianjurkan: \n",
+            "1) Memberikan makanan lunak, blender atau makanan cair seperti susu \n" +
+                    "2) Memberikan makanan yang lunak dan dingin seperti es krim, yoghurt dingin, " +
+                    "atau makanan yang lembut seperti sup krim \n" +
+                    "3) Ingatkan anak untuk sering mencuci mulut untuk membersihkan " +
+                    "sisa makanan dan bakteri sehingga mempercepat penyembuhan \n"
+        ),
+        Tip(
+            6,
+            "Perubahan Indera Perasa",
+            "Salah satu efek terapi adalah menyebabkan perubahan indera perasa, " +
+                    "yang sebaiknya orang tua lakukan adalah: \n",
+            "1) Meningkatkan rasa makanan dengan menambah bumbu, perasa " +
+                    "dan melakukan marinasi atau ungkep sehingga bumbu lebih terserap \n" +
+                    "2) Menawari anak makanan yang dingin \n" +
+                    "3) Memberikan anak minuman yang memiliki rasa \n" +
+                    "4) Menyajikan minuman bersama dengan makanan " +
+                    "untuk menghilangkan rasa yang tidak enak di mulut \n" +
+                    "5) Mendorong anak untuk memperhatikan kebersihan mulut \n"
+        ),
+        Tip(
+            7,
+            "Sembelit/Susah BAB",
+            "Neutropenia adalah kondisi dimana terjadi penurunan " +
+                    "kadar neutrophil (salah satu sel darah putih) dalam darah, " +
+                    "yang menyebabkan anak lebih rentan terkena infeksi, karena itu, " +
+                    "orang tua sebaiknya: \n",
+            "1) Menghindari pemberian daging, ikan dan telur yang mentah atau dimasak " +
+                    "secara tidak matang, susu, yoghurt, jus, dan madu " +
+                    "yang tidak di pasteurisasi/sterilisasi \n" +
+                    "2) Mencuci bersih sayuran dan buah-buah sebelum dikonsumsi \n"
+        ),
+        Tip(
+            8,
+            "Neutropenia",
+            "Neutropenia adalah kondisi dimana terjadi penurunan " +
+                    "kadar neutrophil (salah satu sel darah putih) dalam darah, " +
+                    "yang menyebabkan anak lebih rentan terkena infeksi, karena itu, " +
+                    "orang tua sebaiknya: \n",
+            "1) Menghindari pemberian daging, ikan dan telur yang mentah atau dimasak " +
+                    "secara tidak matang, susu, yoghurt, jus, dan madu " +
+                    "yang tidak di pasteurisasi/sterilisasi \n" +
+                    "2) Mencuci bersih sayuran dan buah-buah sebelum dikonsumsi \n"
+        ),
+        Tip(
+            9,
+            "Peningkatan Nafsu Makan",
+            "",
+            "1) Tetapkan waktu makan, berikan 3x makan utama dan 2x snack " +
+                    "dan pastikan anak memenuhi jadwal tersebut \n" +
+                    "2) Kurangi pemberian makanan tinggi garam untuk mencegah " +
+                    "penumpukan cairan dalam tubuh \n" +
+                    "3) Sajikan sayur dan buah terlebih dahulu pada waktu makan " +
+                    "karena dapat menjaga anak merasa kenyang lebih lama \n" +
+                    "4) Memberikan snack yang sehat, kurangi makanan tinggi gula \n" +
+                    "5) Batasi acara TV atau menonton video yang berkaitan dengan makanan \n" +
+                    "6) Batasi pemberian minuman manis dan jus \n" +
+                    "7) Ajak anak jalan-jalan dan tetap aktif bermain, kurangi penggunaan gadget \n"
+        ),
+    )
+}
