cd2ada2e381dd417890d1e4659b07ce416b6c58c
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-04 14:09:07 +0700

    Model - Rename menu on Menu data source

--- app/src/main/java/xyz/codingwithza/pedionutricare/model/MenuDataSource.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/model/MenuDataSource.kt
@@ -5,7 +5,7 @@ import xyz.codingwithza.pedionutricare.R
 object MenuDataSource {
     val menuItems = listOf(
         Menu(1, R.drawable.ic_launcher_foreground, "Kebutuhan Gizi"),
-        Menu(2, R.drawable.ic_launcher_foreground, "Tips Pemenuhan Gizi"),
+        Menu(2, R.drawable.ic_launcher_foreground, "Tips Pemenuhan Gizi Anak"),
         Menu(3, R.drawable.ic_launcher_foreground, "Bahan Makanan yang Dianjurkan dan Tidak"),
         Menu(4, R.drawable.ic_launcher_foreground, "Bahan Makanan Penukar"),
         Menu(5, R.drawable.ic_launcher_foreground, "Contoh Menu"),
