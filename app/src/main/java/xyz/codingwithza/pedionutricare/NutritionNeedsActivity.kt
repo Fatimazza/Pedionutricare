a25e731fe016353a09b3870a4aaea8e1588cdc10
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-04 11:02:44 +0700

    Needs - Add content Padding

--- app/src/main/java/xyz/codingwithza/pedionutricare/NutritionNeedsActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/NutritionNeedsActivity.kt
@@ -3,14 +3,17 @@ package xyz.codingwithza.pedionutricare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import xyz.codingwithza.pedionutricare.model.NeedsDataSource
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
 
@@ -37,9 +40,13 @@ fun NutritionNeedsScreen() {
 }
 
 @Composable
-fun NutritionNeeds() {
+fun NutritionNeeds(
+    modifier: Modifier = Modifier
+) {
     val needItems = NeedsDataSource.needItems
-    LazyColumn() {
+    LazyColumn(
+        modifier.padding(16.dp)
+    ) {
         items(needItems) { data ->
             Text(data.title)
             Text(data.desc)
