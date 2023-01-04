601465b4e24f0bf01b1f15bb28b940a350298d75
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-04 14:37:21 +0700

    Tips - Display Data from Model as a List

--- app/src/main/java/xyz/codingwithza/pedionutricare/NutritionTipActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/NutritionTipActivity.kt
@@ -3,16 +3,23 @@ package xyz.codingwithza.pedionutricare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import xyz.codingwithza.pedionutricare.model.TipDataSource
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
 
 class NutritionTipActivity : ComponentActivity() {
@@ -44,6 +51,33 @@ fun NutritionTip(
     LazyColumn(
         modifier.padding(16.dp)
     ) {
+        val tipItems = TipDataSource.tipItems
+        items(tipItems) { data ->
+            Text(
+                text = data.title,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
+            if (data.desc.isNotEmpty()) {
+                Text(
+                    text = data.desc,
+                    textAlign = TextAlign.Justify,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 20.sp,
+                    lineHeight = 25.sp,
+                )
+            }
+            Text(
+                text = data.point,
+                textAlign = TextAlign.Justify,
+                fontWeight = FontWeight.Normal,
+                fontSize = 20.sp,
+                lineHeight = 25.sp
+            )
+            Spacer(
+                modifier = modifier.height(20.dp)
+            )
+        }
     }
 }
 
