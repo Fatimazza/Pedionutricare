55035784baab76ac6fa840bffd984c2bb1bff27b
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-04 10:42:49 +0700

    Needs - Display Data from Model as a List

--- app/src/main/java/xyz/codingwithza/pedionutricare/NutritionNeedsActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/NutritionNeedsActivity.kt
@@ -3,8 +3,12 @@ package xyz.codingwithza.pedionutricare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import xyz.codingwithza.pedionutricare.model.NeedsDataSource
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
 
 class NutritionNeedsActivity : ComponentActivity() {
@@ -20,6 +24,17 @@ class NutritionNeedsActivity : ComponentActivity() {
 
 @Composable
 fun NutritionNeedsScreen() {
+    val needItems = NeedsDataSource.needItems
+    LazyColumn(){
+        items(needItems) {
+            data ->
+            Text(data.title)
+            Text(data.desc)
+        }
+        item {
+            Text("Tambahan")
+        }
+    }
 }
 
 @Preview(showBackground = true)
