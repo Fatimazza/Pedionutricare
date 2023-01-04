8133d565954883bb0d5af6fe294ab5dd4b175ad7
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-04 12:53:45 +0700

    Recommendation - Add Lazy Column Layout

--- app/src/main/java/xyz/codingwithza/pedionutricare/FoodRecommendationActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/FoodRecommendationActivity.kt
@@ -3,12 +3,16 @@ package xyz.codingwithza.pedionutricare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
 
 class FoodRecommendationActivity : ComponentActivity() {
@@ -34,8 +38,14 @@ fun FoodRecommendationScreen() {
 }
 
 @Composable
-fun FoodRecommendation() {
-
+fun FoodRecommendation(
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier.padding(16.dp)
+    ) {
+        
+    }
 }
 
 @Preview(showBackground = true)
