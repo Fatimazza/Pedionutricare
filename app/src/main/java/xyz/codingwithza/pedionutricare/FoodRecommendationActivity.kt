809e23ff28eb8613fd0d2cf36bed3aa65a5dc4f9
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-04 13:32:02 +0700

    Recommendation - Display Data from Model as a List

--- app/src/main/java/xyz/codingwithza/pedionutricare/FoodRecommendationActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/FoodRecommendationActivity.kt
@@ -3,16 +3,23 @@ package xyz.codingwithza.pedionutricare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import xyz.codingwithza.pedionutricare.model.RecommendationDataSource
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
 
 class FoodRecommendationActivity : ComponentActivity() {
@@ -44,7 +51,43 @@ fun FoodRecommendation(
     LazyColumn(
         modifier.padding(16.dp)
     ) {
-        
+        val recomItems = RecommendationDataSource.recommendationItems
+        items(recomItems) { data ->
+            Text(
+                text = data.title,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
+            Text(
+                text = "Dianjurkan:",
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 20.sp,
+                lineHeight = 30.sp
+            )
+            Text(
+                text = data.recommended,
+                textAlign = TextAlign.Justify,
+                fontWeight = FontWeight.Normal,
+                fontSize = 20.sp,
+                lineHeight = 25.sp
+            )
+            Text(
+                text = "Tidak Dianjurkan:",
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 20.sp,
+                lineHeight = 30.sp
+            )
+            Text(
+                text = data.notrecommended,
+                textAlign = TextAlign.Justify,
+                fontWeight = FontWeight.Normal,
+                fontSize = 20.sp,
+                lineHeight = 25.sp
+            )
+            Spacer(
+                modifier = modifier.height(20.dp)
+            )
+        }
     }
 }
 
