d1bcf5e3f2497b0c6007f5e2aeb72aaeb12f3e7a
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-04 14:21:00 +0700

    Tips - Add Lazy Column Layout

--- app/src/main/java/xyz/codingwithza/pedionutricare/NutritionTipActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/NutritionTipActivity.kt
@@ -3,12 +3,16 @@ package xyz.codingwithza.pedionutricare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
 
 class NutritionTipActivity : ComponentActivity() {
@@ -34,7 +38,13 @@ fun NutritionTipScreen() {
 }
 
 @Composable
-fun NutritionTip() {
+fun NutritionTip(
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier.padding(16.dp)
+    ) {
+    }
 }
 
 @Preview(showBackground = true)
