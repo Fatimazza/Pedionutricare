7128add2b575e9ae4d51771b5f68c43f42fa3ee8
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-04 10:29:25 +0700

    Model - Add data class and data source for Needs

---
+++ app/src/main/java/xyz/codingwithza/pedionutricare/model/Needs.kt
@@ -0,0 +1,7 @@
+package xyz.codingwithza.pedionutricare.model
+
+data class Needs(
+    val id: Int,
+    val title: String,
+    val desc: String
+)
---
+++ app/src/main/java/xyz/codingwithza/pedionutricare/model/NeedsDataSource.kt
@@ -0,0 +1,10 @@
+package xyz.codingwithza.pedionutricare.model
+
+object NeedsDataSource {
+    val needItems = listOf(
+        Needs(1, "Karbohidrat", "Karbohidrat menjadi sumber energi yang membantu mempertahankan fungsi organ. Contoh sumber karbohidrat adalah nasi, roti, kentang, sereal, jagung dan buah." ),
+        Needs(2, "Protein", "Protein yang cukup membantu anak lebih cepat pulih dari efek samping kemoterapi dan atau radioterapi, serta mencegah infeksi. Sumber protein antara lain daging, ikan, ayam, telur, kacang-kacangan, susu, yoghurt, keju, dll."),
+        Needs(3, "Lemak", "Lemak membantu mempertahankan berat badan pada anak yang mengalami risiko penurunan berat badan selama terapi. Lemak yang tidak jenuh pada ikan, kacang-kacangan, minyak zaitun dan alpukat lebih sehat dibandingkan dengan lemak jenuh dan lemak trans pada daging merah dan makanan yang digoreng."),
+        Needs(4, "Vitamin dan Mineral", "Efek samping kemoterapi dan atau radioterapi kemungkinan menyebabkan kehilangan vitamin dan mineral tertentu, sehingga pemilihan bahan makanan yang tinggi vitamin dan mineral diperlukan untuk memenuhi kebutuhan harian anak.")
+    )
+}
