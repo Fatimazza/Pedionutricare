51a56c616d1dc71ffd41957e65d884ae46c67349
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-04 21:37:39 +0700

    Sample - Display Data from Model as a List of Table

--- app/src/main/java/xyz/codingwithza/pedionutricare/FoodSampleActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/FoodSampleActivity.kt
@@ -3,16 +3,24 @@ package xyz.codingwithza.pedionutricare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import xyz.codingwithza.pedionutricare.model.FoodSampleDataSource
+import xyz.codingwithza.pedionutricare.ui.theme.DarkYellow
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
 
 class FoodSampleActivity : ComponentActivity() {
@@ -38,13 +46,72 @@ fun FoodSampleScreen() {
 }
 
 @Composable
+fun RowScope.TableCell(
+    text: String,
+    weight: Float
+) {
+    Text(
+        text = text,
+        Modifier
+            .border(1.dp, DarkYellow)
+            .weight(weight)
+            .padding(8.dp),
+        fontSize = 20.sp
+    )
+}
+
+@Composable
 fun FoodSample(
     modifier: Modifier = Modifier
 ) {
     LazyColumn(
         modifier.padding(16.dp)
     ) {
-        
+        val foodSamples = FoodSampleDataSource.sampleItems
+
+        item {
+            Text(
+                text = "Contoh Menu Usia 1 - 6 tahun",
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                fontSize = 22.sp,
+                modifier = modifier.fillMaxWidth(),
+            )
+            Spacer(
+                modifier = modifier.height(16.dp)
+            )
+        }
+        items(foodSamples) { data ->
+            Text(
+                text = data.time,
+                textAlign = TextAlign.Justify,
+                fontWeight = FontWeight.Bold,
+                fontSize = 22.sp
+            )
+            data.foods.forEach { item ->
+                Text(
+                    text = item.name,
+                    textAlign = TextAlign.Justify,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 22.sp
+                )
+                Row(Modifier.background(DarkYellow)) {
+                    TableCell(text = "Bahan", weight = 0.5f)
+                    TableCell(text = "gram", weight = 0.2f)
+                    TableCell(text = "URT", weight = 0.3f)
+                }
+                item.ingredients.forEach {
+                    Row(Modifier.fillMaxWidth()) {
+                        TableCell(text = it.bahan, weight = 0.5f)
+                        TableCell(text = it.gram.toString(), weight = 0.2f)
+                        TableCell(text = it.URT, weight = 0.3f)
+                    }
+                }
+            }
+            Spacer(
+                modifier = modifier.height(20.dp)
+            )
+        }
     }
 }
 
