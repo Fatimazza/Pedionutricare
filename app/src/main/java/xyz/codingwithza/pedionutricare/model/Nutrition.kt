cd752092d47743c660e87702066453faaa98d96a
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-04 22:02:49 +0700

    Model - Add Nutrition data class and data source for Needs

---
+++ app/src/main/java/xyz/codingwithza/pedionutricare/model/Nutrition.kt
@@ -0,0 +1,7 @@
+package xyz.codingwithza.pedionutricare.model
+
+data class Nutrition(
+    val id: Int,
+    val nutrition: String,
+    val source: String
+)
---
+++ app/src/main/java/xyz/codingwithza/pedionutricare/model/NutritionDataSource.kt
@@ -0,0 +1,36 @@
+package xyz.codingwithza.pedionutricare.model
+
+object NutritionDataSource {
+    val nutritionItem = listOf(
+        Nutrition(
+            1,
+            "Vitamin C",
+            "Jeruk, lemon, stroberi, tomat, brokoli, kembang kol"
+        ),
+        Nutrition(
+            2,
+            "Vitamin D",
+            "Ikan tuna, kuning telur, hati sapi"
+        ),
+        Nutrition(
+            3,
+            "Kalsium",
+            "Susu dan olahannya, edamame, almond, sayuran hijau"
+        ),
+        Nutrition(
+            4,
+            "Magnesium",
+            "Kacang tanah, kacang mete, kedelai, oatmeal, daging sapi"
+        ),
+        Nutrition(
+            5,
+            "Zat Besi",
+            "Daging merah, hati, seafood, sayuran hijau, kacang-kacangan"
+        ),
+        Nutrition(
+            6,
+            "Zinc",
+            "Daging sapi, ayam, kerang, kepiting, lobster, kacang-kacangan"
+        )
+    )
+}
