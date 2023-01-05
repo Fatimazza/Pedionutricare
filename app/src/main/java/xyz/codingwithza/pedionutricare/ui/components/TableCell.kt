b17789fcddeccb4c03ae853912bd06caa0a95df4
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-05 16:04:00 +0700

    UI Components - Add centered Table Cell

--- app/src/main/java/xyz/codingwithza/pedionutricare/ui/components/TableCell.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/ui/components/TableCell.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import xyz.codingwithza.pedionutricare.ui.theme.DarkYellow
@@ -22,5 +23,21 @@ fun RowScope.TableCell(
             .weight(weight)
             .padding(8.dp),
         fontSize = 20.sp
+    )
+}
+
+@Composable
+fun RowScope.TableCellCenter(
+    text: String,
+    weight: Float
+) {
+    Text(
+        text = text,
+        Modifier
+            .border(1.dp, DarkYellow)
+            .weight(weight)
+            .padding(8.dp),
+        fontSize = 20.sp,
+        textAlign = TextAlign.Center
     )
 }
