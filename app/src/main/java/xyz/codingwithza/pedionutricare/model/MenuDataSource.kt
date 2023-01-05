1cb925e748414a00f2ac1279dd580287a7a048f2
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-05 11:43:44 +0700

    Home - Change Menu Icon

--- app/src/main/java/xyz/codingwithza/pedionutricare/model/MenuDataSource.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/model/MenuDataSource.kt
@@ -4,10 +4,10 @@ import xyz.codingwithza.pedionutricare.R
 
 object MenuDataSource {
     val menuItems = listOf(
-        Menu(1, R.drawable.ic_launcher_foreground, "Kebutuhan Gizi"),
-        Menu(2, R.drawable.ic_launcher_foreground, "Tips Pemenuhan Gizi Anak"),
-        Menu(3, R.drawable.ic_launcher_foreground, "Bahan Makanan yang Dianjurkan dan Tidak"),
-        Menu(4, R.drawable.ic_launcher_foreground, "Bahan Makanan Penukar"),
-        Menu(5, R.drawable.ic_launcher_foreground, "Contoh Menu"),
+        Menu(1, R.drawable.ic_outline_restaurant_24, "Kebutuhan Gizi"),
+        Menu(2, R.drawable.ic_outline_restaurant_24, "Tips Pemenuhan Gizi Anak"),
+        Menu(3, R.drawable.ic_outline_restaurant_24, "Bahan Makanan yang Dianjurkan dan Tidak"),
+        Menu(4, R.drawable.ic_outline_restaurant_24, "Bahan Makanan Penukar"),
+        Menu(5, R.drawable.ic_outline_restaurant_24, "Contoh Menu"),
     )
 }
--- app/src/main/java/xyz/codingwithza/pedionutricare/ui/components/ButtonImageMenu.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/ui/components/ButtonImageMenu.kt
@@ -3,6 +3,7 @@ package xyz.codingwithza.pedionutricare.ui.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -10,8 +11,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import xyz.codingwithza.pedionutricare.R
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
@@ -21,17 +24,20 @@ fun ButtonImageMenu(
     image: Int,
     title: String,
     onClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
     Button(onClick = onClick) {
         Row(
-            modifier = Modifier
+            modifier = modifier
                 .fillMaxWidth()
                 .wrapContentHeight(),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Image(
+                modifier = modifier.padding(28.dp),
                 painter = painterResource(image),
-                contentDescription = title
+                contentDescription = title,
+                contentScale = ContentScale.FillWidth
             )
             Text(
                 text = title,
@@ -48,7 +54,7 @@ fun ButtonImageMenu(
 fun ButtonImagePreview() {
     PedionutricareTheme {
         ButtonImageMenu(
-            R.drawable.ic_launcher_foreground,
+            R.drawable.ic_outline_restaurant_24,
             "This is Menu Long Menu Long 2",
             onClick = {})
     }
