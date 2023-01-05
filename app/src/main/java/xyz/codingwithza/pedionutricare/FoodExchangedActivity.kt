c55a4cb0a63efb1cebd714a7ce81d688766f73a3
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-05 17:07:18 +0700

    Exchanged - Add Scaffold Layout

--- app/src/main/java/xyz/codingwithza/pedionutricare/FoodExchangedActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/FoodExchangedActivity.kt
@@ -3,7 +3,11 @@ package xyz.codingwithza.pedionutricare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
 
@@ -20,6 +24,13 @@ class FoodExchangedActivity : ComponentActivity() {
 
 @Composable
 fun FoodExchangedScreen() {
+    Scaffold(topBar = {
+        TopAppBar(title = {
+            Text(stringResource(R.string.app_name))
+        })
+    }) {
+        FoodExchanged()
+    }
 }
 
 @Composable
