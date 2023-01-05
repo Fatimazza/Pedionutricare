d2da0fe2202c408780143fd568a94033711b9531
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-05 15:50:06 +0700

    Model - Update Fluids data source

--- app/src/main/java/xyz/codingwithza/pedionutricare/model/FluidsDataSource.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/model/FluidsDataSource.kt
@@ -4,37 +4,37 @@ object FluidsDataSource {
     val fluidItems = listOf(
         Fluids(
             1,
-            "0-6 bulan",
-            "700 ml/hari",
-            "700 ml/hari"
+            "0 - 6 bulan",
+            "700 ml/hari \n",
+            "700 ml/hari \n"
         ),
         Fluids(
             2,
-            "7-12 bulan",
-            "800 ml/hari",
-            "800 ml/hari"
+            "7 - 12 bulan",
+            "800 ml/hari \n",
+            "800 ml/hari \n"
         ),
         Fluids(
             3,
-            "1-3 tahun",
+            "1 - 3 tahun",
             "1300 ml/hari",
             "1300 ml/hari"
         ),
         Fluids(
             4,
-            "4-8 tahun",
+            "4 - 8 tahun",
             "1700 ml/hari",
             "1700 ml/hari"
         ),
         Fluids(
             5,
-            "9-13 tahun",
+            "9 - 13 tahun",
             "2400 ml/hari",
             "2100 ml/hari"
         ),
         Fluids(
             6,
-            "14-18 tahun",
+            "14 - 18 tahun",
             "3300 ml/hari",
             "2300 ml/hari"
         )
