52215c19c98f90c1a80dee9e3e6e15d619a20504
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-05 15:35:56 +0700

    Model - Add Fluids data class and data source for Needs

---
+++ app/src/main/java/xyz/codingwithza/pedionutricare/model/Fluids.kt
@@ -0,0 +1,8 @@
+package xyz.codingwithza.pedionutricare.model
+
+data class Fluids(
+    val id: Int,
+    val age: String,
+    val male: String,
+    val female: String
+)
---
+++ app/src/main/java/xyz/codingwithza/pedionutricare/model/FluidsDataSource.kt
@@ -0,0 +1,42 @@
+package xyz.codingwithza.pedionutricare.model
+
+class FluidsDataSource {
+    val fluidItems = listOf(
+        Fluids(
+            1,
+            "0-6 bulan",
+            "700 ml/hari",
+            "700 ml/hari"
+        ),
+        Fluids(
+            2,
+            "7-12 bulan",
+            "800 ml/hari",
+            "800 ml/hari"
+        ),
+        Fluids(
+            3,
+            "1-3 tahun",
+            "1300 ml/hari",
+            "1300 ml/hari"
+        ),
+        Fluids(
+            4,
+            "4-8 tahun",
+            "1700 ml/hari",
+            "1700 ml/hari"
+        ),
+        Fluids(
+            5,
+            "9-13 tahun",
+            "2400 ml/hari",
+            "2100 ml/hari"
+        ),
+        Fluids(
+            6,
+            "14-18 tahun",
+            "3300 ml/hari",
+            "2300 ml/hari"
+        )
+    )
+}
