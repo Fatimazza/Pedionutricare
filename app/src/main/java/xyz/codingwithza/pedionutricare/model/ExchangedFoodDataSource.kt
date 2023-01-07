f8015d6ecb07ff3537bdc6455c9a36b7929041e9
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-07 18:58:15 +0700

    Model - Add more data source for Exchanged Food

--- app/src/main/java/xyz/codingwithza/pedionutricare/model/ExchangedFoodDataSource.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/model/ExchangedFoodDataSource.kt
@@ -15,13 +15,91 @@ object ExchangedFoodDataSource {
                             1,
                             "Bihun",
                             50,
-                            "1/2 gls"
+                            "½ gls"
                         ),
                         Ingredients(
                             2,
                             "Bubur Beras",
                             400,
                             "2 gls"
+                        ),
+                        Ingredients(
+                            3,
+                            "Biskuit",
+                            40,
+                            "4 bh bsr"
+                        ),
+                        Ingredients(
+                            4,
+                            "Havermouth",
+                            45,
+                            "5 ½ sdm"
+                        ),
+                        Ingredients(
+                            5,
+                            "Kentang",
+                            210,
+                            "2 bj sdg"
+                        ),
+                        Ingredients(
+                            6,
+                            "Krackers",
+                            50,
+                            "5 bh sdg"
+                        ),
+                        Ingredients(
+                            7,
+                            "Makaroni",
+                            50,
+                            "½ gls"
+                        ),
+                        Ingredients(
+                            8,
+                            "Mi basah",
+                            200,
+                            "2 gls"
+                        ),
+                        Ingredients(
+                            9,
+                            "Mi Kering",
+                            50,
+                            "1 gls"
+                        ),
+                        Ingredients(
+                            10,
+                            "Nasi",
+                            100,
+                            "¾ gls"
+                        ),
+                        Ingredients(
+                            11,
+                            "Nasi Tim",
+                            200,
+                            "1 gls"
+                        ),
+                        Ingredients(
+                            12,
+                            "Roti Putih",
+                            70,
+                            "3 iris"
+                        ),
+                        Ingredients(
+                            13,
+                            "Singkong",
+                            120,
+                            "1 ptg"
+                        ),
+                        Ingredients(
+                            14,
+                            "Talas",
+                            125,
+                            "1 ptg"
+                        ),
+                        Ingredients(
+                            15,
+                            "Tepung Beras",
+                            150,
+                            "8 sdm"
                         )
                     )
                 )
@@ -43,10 +121,16 @@ object ExchangedFoodDataSource {
                             "1 gls"
                         ),
                         Ingredients(
-                            1,
+                            2,
                             "Tepung susu skim",
                             20,
                             "4 sdm"
+                        ),
+                        Ingredients(
+                            3,
+                            "Yogurt non fat",
+                            120,
+                            "⅔ gls"
                         )
                     )
                 ),
@@ -65,7 +149,25 @@ object ExchangedFoodDataSource {
                             2,
                             "Susu Kambing",
                             165,
-                            "3/4 gls"
+                            "¾ gls"
+                        ),
+                        Ingredients(
+                            3,
+                            "Susu Sapi",
+                            200,
+                            "1 gls"
+                        ),
+                        Ingredients(
+                            4,
+                            "Susu Kental Manis",
+                            100,
+                            "½ gls"
+                        ),
+                        Ingredients(
+                            5,
+                            "Yogurt Susu",
+                            100,
+                            "1 gls"
                         )
                     )
                 )
