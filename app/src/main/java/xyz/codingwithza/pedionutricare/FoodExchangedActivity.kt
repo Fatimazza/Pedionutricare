795011a6cf6ecd1da40d74c2a89c7f8d0cfa4c46
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-09 19:39:21 +0700

    Exchanged - Add Card Layout

--- app/src/main/java/xyz/codingwithza/pedionutricare/FoodExchangedActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/FoodExchangedActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import xyz.codingwithza.pedionutricare.model.ExchangeFood
 import xyz.codingwithza.pedionutricare.model.ExchangeFoodDataSource
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
@@ -43,7 +48,6 @@ fun FoodExchangedScreen() {
             items(exchangedItems) { data ->
                 FoodExchangedItem(exchangedItems, data)
             }
-
         }
     }
 }
@@ -54,7 +58,25 @@ fun FoodExchangedItem(
     data: ExchangeFood,
     modifier: Modifier = Modifier
 ) {
-
+    Card(
+        modifier = modifier.padding(8.dp),
+        elevation = 4.dp
+    ) {
+        Column() {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(
+                    text = data.title,
+                    textAlign = TextAlign.Justify,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
