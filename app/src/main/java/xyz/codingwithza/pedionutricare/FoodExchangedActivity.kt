c26060dbffc903ee766b08a999f4dbef18327e1f
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-09 17:57:10 +0700

    Exchanged - Add Lazy Column Layout

--- app/src/main/java/xyz/codingwithza/pedionutricare/FoodExchangedActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/FoodExchangedActivity.kt
@@ -3,12 +3,19 @@ package xyz.codingwithza.pedionutricare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import xyz.codingwithza.pedionutricare.model.ExchangeFood
+import xyz.codingwithza.pedionutricare.model.ExchangeFoodDataSource
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
 
 class FoodExchangedActivity : ComponentActivity() {
@@ -29,8 +36,25 @@ fun FoodExchangedScreen() {
             Text(stringResource(R.string.app_name))
         })
     }) {
+        val exchangedItems = ExchangeFoodDataSource.exchangedItems
+        LazyColumn(
+            modifier = Modifier.background(MaterialTheme.colors.background)
+        ) {
+            items(exchangedItems) { data ->
+                FoodExchangedItem(exchangedItems, data)
+            }
 
+        }
     }
+}
+
+@Composable
+fun FoodExchangedItem(
+    needItems: List<ExchangeFood>,
+    data: ExchangeFood,
+    modifier: Modifier = Modifier
+) {
+
 }
 
 @Preview(showBackground = true)
