e7fcaaddba52164b81d02f367af16494b5b4ebc3
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-11 11:33:51 +0700

    Recommendation - Move Lazy Column Layout

--- app/src/main/java/xyz/codingwithza/pedionutricare/FoodRecommendationActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/FoodRecommendationActivity.kt
@@ -3,9 +3,9 @@ package xyz.codingwithza.pedionutricare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
@@ -19,8 +19,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import xyz.codingwithza.pedionutricare.model.Recommendation
 import xyz.codingwithza.pedionutricare.model.RecommendationDataSource
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
+import xyz.codingwithza.pedionutricare.ui.theme.Yellow_Awake
 
 class FoodRecommendationActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,61 +36,65 @@ class FoodRecommendationActivity : ComponentActivity() {
 }
 
 @Composable
-fun FoodRecommendationScreen() {
+fun FoodRecommendationScreen(
+    modifier: Modifier = Modifier
+) {
     Scaffold(topBar = {
         TopAppBar(title = {
             Text(stringResource(R.string.app_name))
         })
     }) {
-        FoodRecommendation()
+        LazyColumn(
+            modifier.background(Yellow_Awake)
+        ) {
+            val recomItems = RecommendationDataSource.recommendationItems
+            items(recomItems) { data ->
+                FoodRecommendation(recomItems, data)
+            }
+        }
     }
 }
 
 @Composable
 fun FoodRecommendation(
+    recomItems: List<Recommendation>,
+    data: Recommendation,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(
-        modifier.padding(16.dp)
-    ) {
-        val recomItems = RecommendationDataSource.recommendationItems
-        items(recomItems) { data ->
-            Text(
-                text = data.title,
-                fontWeight = FontWeight.Bold,
-                fontSize = 24.sp
-            )
-            Text(
-                text = "Dianjurkan:",
-                fontWeight = FontWeight.SemiBold,
-                fontSize = 20.sp,
-                lineHeight = 30.sp
-            )
-            Text(
-                text = data.recommended,
-                textAlign = TextAlign.Justify,
-                fontWeight = FontWeight.Normal,
-                fontSize = 20.sp,
-                lineHeight = 25.sp
-            )
-            Text(
-                text = "Tidak Dianjurkan:",
-                fontWeight = FontWeight.SemiBold,
-                fontSize = 20.sp,
-                lineHeight = 30.sp
-            )
-            Text(
-                text = data.notrecommended,
-                textAlign = TextAlign.Justify,
-                fontWeight = FontWeight.Normal,
-                fontSize = 20.sp,
-                lineHeight = 25.sp
-            )
-            Spacer(
-                modifier = modifier.height(20.dp)
-            )
-        }
-    }
+    Text(
+        text = data.title,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    )
+    Text(
+        text = "Dianjurkan:",
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 30.sp
+    )
+    Text(
+        text = data.recommended,
+        textAlign = TextAlign.Justify,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+        lineHeight = 25.sp
+    )
+    Text(
+        text = "Tidak Dianjurkan:",
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 30.sp
+    )
+    Text(
+        text = data.notrecommended,
+        textAlign = TextAlign.Justify,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+        lineHeight = 25.sp
+    )
+    Spacer(
+        modifier = modifier.height(20.dp)
+    )
 }
 
 @Preview(showBackground = true)
