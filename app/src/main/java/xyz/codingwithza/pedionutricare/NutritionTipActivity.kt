806c2eb7ae3ee912329f66035a04be27e590f6cb
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-11 12:25:08 +0700

    Tips - Add Title text max line and overflow

--- app/src/main/java/xyz/codingwithza/pedionutricare/NutritionTipActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/NutritionTipActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -81,7 +82,9 @@ fun NutritionTip(
                     text = data.title,
                     textAlign = TextAlign.Justify,
                     fontWeight = FontWeight.Bold,
-                    fontSize = 22.sp
+                    fontSize = 22.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
                 Spacer(Modifier.weight(1f))
                 NutritionTipButton(
