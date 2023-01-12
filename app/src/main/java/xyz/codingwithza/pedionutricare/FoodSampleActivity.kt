1da5b016f701f66a6503b12aff28c3fd399a7a65
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-12 17:16:43 +0700

    Sample - Show Image for age 1 to 6

--- app/src/main/java/xyz/codingwithza/pedionutricare/FoodSampleActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/FoodSampleActivity.kt
@@ -3,6 +3,7 @@ package xyz.codingwithza.pedionutricare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
@@ -11,7 +12,9 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -75,6 +78,15 @@ fun FoodSample(
             )
             Spacer(
                 modifier = modifier.height(16.dp)
+            )
+            val painter = painterResource(id = R.drawable.img_sample_1ke6)
+            Image(
+                painter = painter,
+                contentDescription = "Exchanged Food Image",
+                modifier = modifier
+                    .aspectRatio(painter.intrinsicSize.width / painter.intrinsicSize.height)
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Fit
             )
         }
     }
