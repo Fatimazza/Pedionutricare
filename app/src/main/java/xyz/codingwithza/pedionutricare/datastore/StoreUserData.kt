afd8f078527dc32b2c8d2923093fdb4fbb9fc9d2
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-12 14:42:49 +0700

    Data Store - Add function to Save data into DataStore

--- app/src/main/java/xyz/codingwithza/pedionutricare/datastore/StoreUserData.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/datastore/StoreUserData.kt
@@ -3,11 +3,12 @@ package xyz.codingwithza.pedionutricare.datastore
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 
-class StoreUserData {
+class StoreUserData(private val context: Context) {
 
     // to make sure there is only one instance
     companion object {
@@ -15,5 +16,19 @@ class StoreUserData {
 
         val USER_NAME_KEY = stringPreferencesKey("user_name")
         val USER_AGE_KEY = intPreferencesKey("user_age")
+    }
+
+    //save data into datastore
+
+    suspend fun saveUserName(name: String) {
+        context.dataStore.edit { preferences ->
+            preferences[USER_NAME_KEY] = name
+        }
+    }
+
+    suspend fun saveUserAge(age: Int) {
+        context.dataStore.edit { preferences ->
+            preferences[USER_AGE_KEY] = age
+        }
     }
 }
