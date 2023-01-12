e6086036a90d42b41c502c3001e763b4c7879a8b
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-12 14:41:42 +0700

    Data Store - Create Preferences DataStore

--- app/src/main/java/xyz/codingwithza/pedionutricare/datastore/StoreUserData.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/datastore/StoreUserData.kt
@@ -1,5 +1,19 @@
 package xyz.codingwithza.pedionutricare.datastore
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+
 class StoreUserData {
-    
+
+    // to make sure there is only one instance
+    companion object {
+        private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("userData")
+
+        val USER_NAME_KEY = stringPreferencesKey("user_name")
+        val USER_AGE_KEY = intPreferencesKey("user_age")
+    }
 }
