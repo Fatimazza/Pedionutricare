8d15ef5c32608fb195b6481d235edb2b4bd30d9e
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-12 14:02:16 +0700

    Login - Add Texts and Space

--- app/src/main/java/xyz/codingwithza/pedionutricare/LoginActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/LoginActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
@@ -33,7 +36,20 @@ fun LoginScreen(
             .fillMaxSize()
             .padding(16.dp, 16.dp),
     ) {
-
+        Text(
+            text = "Selamat Datang di",
+            style = MaterialTheme.typography.h5,
+            modifier = modifier.padding(4.dp)
+        )
+        Text(
+            text = "Pedionutricare",
+            style = MaterialTheme.typography.h3,
+            fontWeight = FontWeight.Light,
+            modifier = modifier.padding(8.dp)
+        )
+        Spacer(
+            modifier = modifier.height(16.dp)
+        )
     }
 }
 
