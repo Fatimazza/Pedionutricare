3d21069ddb46de5510923f3663b3704d98ad0d8d
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-13 11:49:39 +0700

    Model - Change Image on Menu data source

--- app/src/main/java/xyz/codingwithza/pedionutricare/model/MenuDataSource.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/model/MenuDataSource.kt
@@ -4,10 +4,10 @@ import xyz.codingwithza.pedionutricare.R
 
 object MenuDataSource {
     val menuItems = listOf(
-        Menu(1, R.drawable.ic_outline_restaurant_24, "Kebutuhan Gizi"),
-        Menu(2, R.drawable.ic_outline_restaurant_24, "Tips Pemenuhan Gizi Anak"),
-        Menu(3, R.drawable.ic_outline_restaurant_24, "Bahan Makanan yang Dianjurkan dan Tidak"),
-        Menu(4, R.drawable.ic_outline_restaurant_24, "Bahan Makanan Penukar"),
-        Menu(5, R.drawable.ic_outline_restaurant_24, "Contoh Menu"),
+        Menu(1, R.drawable.img_menu_need, "Kebutuhan Gizi"),
+        Menu(2, R.drawable.img_menu_tips, "Tips Pemenuhan Gizi Anak"),
+        Menu(3, R.drawable.img_menu_recommended, "Bahan Makanan yang Dianjurkan dan Tidak"),
+        Menu(4, R.drawable.img_menu_exchanged, "Bahan Makanan Penukar"),
+        Menu(5, R.drawable.img_menu_sample, "Contoh Menu"),
     )
 }
