20378312761fe493294ecbd7335f15c590b7ee45
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-13 17:47:06 +0700

    System UI Theme - Add Accompanist Dependency and its implementation

--- app/src/main/java/xyz/codingwithza/pedionutricare/ui/theme/Theme.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/ui/theme/Theme.kt
@@ -5,6 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -55,5 +57,13 @@ fun PedionutricareTheme(
         typography = Typography,
         shapes = Shapes,
         content = content
+    )
+
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(
+        color = if (darkTheme) DarkGray else Yellow
+    )
+    systemUiController.setStatusBarColor(
+        color = if (darkTheme) DarkGray else Yellow
     )
 }
