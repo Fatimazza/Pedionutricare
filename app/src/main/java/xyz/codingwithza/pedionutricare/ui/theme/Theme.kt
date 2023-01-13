3c9cf29a68c32b8fbb21a1d9dc5efe3034b1b2b2
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-01-13 17:50:28 +0700

    Theme - Remove unused default Theme

--- app/src/main/java/xyz/codingwithza/pedionutricare/ui/theme/Theme.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/ui/theme/Theme.kt
@@ -5,37 +5,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
-
-private val DarkYellowColorPalette = darkColors(
     primary = DarkYellow,
     primaryVariant = Yellow_Deep,
     secondary = DarkGray
 )
 
-private val LightYellowColorPalette = lightColors(
+private val LightColorPalette = lightColors(
     primary = Yellow,
     primaryVariant = Yellow_Deep,
     secondary = White
@@ -47,9 +25,9 @@ fun PedionutricareTheme(
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
-        DarkYellowColorPalette
+        DarkColorPalette
     } else {
-        LightYellowColorPalette
+        LightColorPalette
     }
 
     MaterialTheme(
