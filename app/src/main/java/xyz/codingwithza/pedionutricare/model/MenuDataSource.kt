b57b1220b7f63de8392e9a939dccd41701cd41ff
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-03-25 11:20:54 +0700

    Model - Add daily menu on Menu data source

--- app/src/main/java/xyz/codingwithza/pedionutricare/model/MenuDataSource.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/model/MenuDataSource.kt
@@ -4,10 +4,11 @@ import xyz.codingwithza.pedionutricare.R
 
 object MenuDataSource {
     val menuItems = listOf(
-        Menu(1, R.drawable.img_menu_need, "Kebutuhan Gizi"),
+        Menu(1, R.drawable.img_menu_need, "Kebutuhan Zat Gizi"),
         Menu(2, R.drawable.img_menu_tips, "Tips Pemenuhan Gizi Anak"),
         Menu(3, R.drawable.img_menu_recommended, "Bahan Makanan yang Dianjurkan dan Tidak"),
         Menu(4, R.drawable.img_menu_exchanged, "Bahan Makanan Penukar"),
         Menu(5, R.drawable.img_menu_sample, "Contoh Menu"),
+        Menu(6, R.drawable.img_menu_daily, "Kebutuhan Bahan Makanan Sehari")
     )
 }
