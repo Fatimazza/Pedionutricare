ad126fa311d91b40e3c35acfa99a9c0ab034aabf
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-03-25 15:46:18 +0700

    Daily - Add Outlined Text for Name and Age

--- app/src/main/java/xyz/codingwithza/pedionutricare/DailyNeedsActivity.kt
+++ app/src/main/java/xyz/codingwithza/pedionutricare/DailyNeedsActivity.kt
@@ -5,15 +5,22 @@ import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import xyz.codingwithza.pedionutricare.ui.theme.DarkGray
 import xyz.codingwithza.pedionutricare.ui.theme.PedionutricareTheme
+import xyz.codingwithza.pedionutricare.ui.theme.Yellow_Awake
+import xyz.codingwithza.pedionutricare.ui.theme.Yellow_Deep
 
 class DailyNeedsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,7 +49,48 @@ fun DailyNeedsScreen(
         val context = LocalContext.current
         ActivityInfo.SCREEN_ORIENTATION_PORTRAIT.also { (context as? Activity)?.requestedOrientation = it }
 
-        DailyNeeds()
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier
+                .fillMaxSize()
+                .background(Yellow_Awake)
+                .padding(8.dp, 8.dp),
+        ) {
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                placeholder = { Text(text = "Nama Anda") },
+                label = { Text("Masukkan Nama") },
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Yellow_Deep,
+                    unfocusedBorderColor = DarkGray,
+                    focusedLabelColor = Yellow_Deep
+                ),
+                modifier = Modifier
+                    .padding(16.dp, 0.dp, 16.dp, 0.dp)
+                    .fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                placeholder = { Text(text = "10 Tahun") },
+                label = { Text("Masukkan Umur") },
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Yellow_Deep,
+                    unfocusedBorderColor = DarkGray,
+                    focusedLabelColor = Yellow_Deep
+                ),
+                modifier = Modifier
+                    .padding(16.dp, 0.dp, 16.dp, 0.dp)
+                    .fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                )
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+        }
     }
 }
 
